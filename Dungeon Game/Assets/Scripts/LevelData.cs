dc1d189fd677cbed38d443296b7269d63db78c53
Clicky02 <garethfultz@gmail.com>
2019-04-02 13:07:09 -0400

    Update LevelData.cs

--- Dungeon Game/Assets/Scripts/LevelData.cs
+++ Dungeon Game/Assets/Scripts/LevelData.cs
@@ -1,1 +1,12 @@
+    
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
+public class GameData : MonoBehaviour
+{
+
+}
