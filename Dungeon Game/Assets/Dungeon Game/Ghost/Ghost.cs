8a8cd35c810f9181e010fcc8c99e7605c957a00c
Clicky02 <garethfultz@gmail.com>
2019-04-04 13:08:19 -0400

    Update Ghost.cs

--- Dungeon Game/Assets/Dungeon Game/Ghost/Ghost.cs
+++ Dungeon Game/Assets/Dungeon Game/Ghost/Ghost.cs
@@ -6,6 +6,7 @@ public class Ghost : HealthEntity
 {
 
     Random r = new Random();
+    public int sightRange = 5;
 
     // Start is called before the first frame update
     public override void Start()
@@ -23,24 +24,35 @@ public class Ghost : HealthEntity
 
     public override void Act()
     {
-        switch (Mathf.Floor(Random.value * 4))
+        HealthEntity target = null;
+        int cDistX = 1000;
+        int cDistY = 1000;
+        int cEX = 1000;
+        int cEY = 1000;
+        foreach(Entity e in myDictionary.Values)
         {
-            case 0:
-                Move(Vector3.up);
-                break;
-            case 1:
-                Move(Vector3.down);
-                break;
-            case 2:
-                Move(Vector3.right);
-                break;
-            case 3:
-                Move(Vector3.left);
-                break;
-            default:
-                Move(Vector3.down);
-                break;
+            HealthEntiy h = e as HealthEnity;
+            if (h != null && h.team != team)
+            {
+                int distX = h.tilePos.x - tilePos.x;
+                int distY = h.tilePos.y - tilePos.y;
+                if (Mathf.Abs(distX) <= sightRange && Mathf.Abs(distY) <= sightRange)
+                {
+                    eX = distX*direction.x + distY*direction.y;
+                    eY = distY*direction.x + distX*direction.y;
+                    if (0 < eX && Mathf.abs(eY) <= eX && eX < cEX)
+                    {
+                        target = h;
+                        cDistX = distX;
+                        cDistY = distY;
+                        cEX = eX;
+                        cEY = eY;
+                    }
+                }
+            }
         }
+        
+        
 
     }
 }
