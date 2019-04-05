041e52be3f5f6e121df846580e73636c39a08810
Clicky02 <garethfultz@gmail.com>
2019-04-05 11:09:14 -0400

    Save Changes
    IDRK if this'll help with the bug, but hopefully it will

--- Dungeon Game/Assets/Scripts/GameData.cs
+++ Dungeon Game/Assets/Scripts/GameData.cs
@@ -76,6 +76,38 @@ public class GameData : MonoBehaviour
         file.Close();
 
     }
+    
+    public void SaveBackup()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerDataBackup.dat", FileMode.OpenOrCreate);
+
+        PlayerData data = new PlayerData
+        {
+            level = level,
+            experience = experience,
+            gold = gold,
+
+            baseHealth = baseHealth,
+            baseMana = baseMana,
+
+            helmet = helmet,
+            chestplate = chestplate,
+            boots = boots,
+            weapon = weapon,
+
+            classType = classType,
+
+            inventory = inventory,
+            unlockedAbilities = unlockedAbilities,
+            basicAbility = basicAbility,
+            equippedAbilities = equippedAbilities
+        };
+
+        bf.Serialize(file, data);
+        file.Close();
+
+    }
 
     public void OnDisable()
     {
@@ -90,30 +122,86 @@ public class GameData : MonoBehaviour
             FileStream file = File.OpenRead(Application.persistentDataPath + "/playerData.dat");
             PlayerData data = (PlayerData)bf.Deserialize(file);
             file.Close();
+            ResetInventory();
+            if (data.level != null)
             level = data.level;
+            if (data.experience != null)
             experience = data.experience;
+            if (data.gold != null)
             gold = data.gold;
 
+            if (data.classType != null)
             classType = data.classType;
 
+            if (data.baseHealth != null)
             baseHealth = data.baseHealth;
+            if (data.baseMana != null)
             baseMana = data.baseMana;
-
+            
+            if (data.helmet != null)
             helmet = data.helmet;
+            if (data.chestplate != null)
             chestplate = data.chestplate;
+            if (data.boots != null)
             boots = data.boots;
-            weapon = data.weapon;
-
+            if (data.weapon != null)
+            weapon = data.weapon
+            
+            if (data.inventory != null)
             inventory = data.inventory;
+            if (data.unlockedAbilities != null)
             unlockedAbilities = data.unlockedAbilities;
+            if (data.basicAbility != null)
             basicAbility = data.basicAbility;
+            if (data.equippedAbilities != null)
             equippedAbilities = data.equippedAbilities;
 
+        }
+        else if (File.Exists(Application.persistentDataPath + "/playerDataBackup.dat")) 
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.OpenRead(Application.persistentDataPath + "/playerDataBackup.dat");
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+            ResetInventory();
+            if (data.level != null)
+            level = data.level;
+            if (data.experience != null)
+            experience = data.experience;
+            if (data.gold != null)
+            gold = data.gold;
+
+            if (data.classType != null)
+            classType = data.classType;
+
+            if (data.baseHealth != null)
+            baseHealth = data.baseHealth;
+            if (data.baseMana != null)
+            baseMana = data.baseMana;
+            
+            if (data.helmet != null)
+            helmet = data.helmet;
+            if (data.chestplate != null)
+            chestplate = data.chestplate;
+            if (data.boots != null)
+            boots = data.boots;
+            if (data.weapon != null)
+            weapon = data.weapon
+            
+            if (data.inventory != null)
+            inventory = data.inventory;
+            if (data.unlockedAbilities != null)
+            unlockedAbilities = data.unlockedAbilities;
+            if (data.basicAbility != null)
+            basicAbility = data.basicAbility;
+            if (data.equippedAbilities != null)
+            equippedAbilities = data.equippedAbilities;
         }
         else
         {
             ResetInventory();
         }
+        SaveBackup();
     }
 
     public void ResetInventory() 
